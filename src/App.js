5fa71bfeb7631a94970376b046de08c1982a5e96
esraamalkawi <esraamalkawi462@gmail.com>
2021-06-01 01:27:33 -0700

    cake shop

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+  <div> 
+      <p className = "head">
+        Esra'a Cake Shop
+      </p>
+      <p className = "desc">
+      we are given your events a magical taste
+      </p>
+      <p> 
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJRvKUwWGAqtbwwvc1V4LPRVcCr4f_rVSwmQ&usqp=CAU" alt="The Cake Shop"  className ="img" />
+    </p>
+    </div>
+  );
+}
+
+export default App;
