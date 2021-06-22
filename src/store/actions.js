8fc36b70237cdc7c5ca5dcf6fbcd10893de27361
esraamalkawi <esraamalkawi462@gmail.com>
2021-06-22 13:24:04 -0700

    conect backend with frontend

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,28 +1,69 @@
+import axios from "axios";
+
 export const DELETE_PRODUCT = "DELETE_PRODUCT";
 export const CREATE_PRODUCT = "ADD_PRODUCT";
 export const UPDATE_PRODUCT = "UPDATE_PRODUCT";
+export const FETCH_PRODUCT = "FETCH_PRODUCT";
+
 export const deleteProduct = (productId) => {
-  return {
-    type: DELETE_PRODUCT,
-    payload: {
-      productId: productId,
-    },
+  return async (dispatch) => {
+    try {
+      await axios.delete(`http://localhost:8000/products/${productId}`);
+      dispatch({
+        type: DELETE_PRODUCT,
+        payload: {
+          productId: productId,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
 };
 export const addProduct = (newProduct) => {
-  return {
-    type: CREATE_PRODUCT,
-    payload: {
-      newProduct: newProduct,
-    },
+  return async (dispatch) => {
+    try {
+      const res = await axios.post(
+        "http://localhost:8000/products",
+        newProduct
+      );
+      dispatch({
+        type: CREATE_PRODUCT,
+        payload: res.data,
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
 };
 
 export const updateProduct = (updatedProduct) => {
-  return {
-    type: UPDATE_PRODUCT,
-    payload: {
-      updatedProduct: updatedProduct,
-    },
+  return async (dispatch) => {
+    try {
+      const res = await axios.put(
+        `http://localhost:8000/products/${updatedProduct.id}`,
+        updatedProduct
+      );
+      dispatch({
+        type: UPDATE_PRODUCT,
+        payload: { updatedProduct: res.data },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
+
+export const fetchProducts = () => {
+  return async (dispatch) => {
+    try {
+      const res = await axios.get("http://localhost:8000/products");
+      dispatch({
+        type: FETCH_PRODUCT,
+        payload: res.data,
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
 };
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,8 +1,12 @@
-import productsData from "../products";
-import { CREATE_PRODUCT, DELETE_PRODUCT, UPDATE_PRODUCT } from "./actions";
+import {
+  CREATE_PRODUCT,
+  DELETE_PRODUCT,
+  FETCH_PRODUCT,
+  UPDATE_PRODUCT,
+} from "./actions";
 import slugify from "slugify";
 const initialState = {
-  products: productsData,
+  products: [],
 };
 const reducer = (state = initialState, action) => {
   switch (action.type) {
@@ -15,30 +19,23 @@ const reducer = (state = initialState, action) => {
         products: keepProduct,
       };
     case CREATE_PRODUCT:
-      action.payload.newProduct.id =
-        state.products[state.products.length - 1].id + 1;
-      action.payload.newProduct.slug = slugify(action.payload.newProduct.name);
       return {
         ...state,
-        products: [...state.products, action.payload.newProduct],
+        products: [...state.products, action.payload],
       };
     case UPDATE_PRODUCT:
       const { updatedProduct } = action.payload;
-      updatedProduct.slug = slugify(updatedProduct.name);
       return {
         ...state,
         products: state.products.map((product) =>
           product.id === updatedProduct.id ? updatedProduct : product
         ),
       };
-    // const editProduct = state.products.map((product) => {
-    //   if (product.id === action.payload.updatedProduct.id) return product;
-    //   else return action.payload.updatedProduct;
-    // });
-    // return {
-    //   ...state,
-    //   products: state.products,
-    // };
+    case FETCH_PRODUCT:
+      return {
+        ...state,
+        products: action.payload,
+      };
     default:
       return state;
   }
